b841bcb4ac3606ec36add3baeb0360c11ae66db8
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-11-20 17:13:13 +0530

    Added fetching single post code

--- graphql/schema.js
+++ graphql/schema.js
@@ -51,7 +51,8 @@ module.exports = buildSchema(`
 
     type RootQuery{
         login(email : String!, password : String!) : AuthData!,
-        posts(page : Int!) : PostData!
+        posts : PostData!,
+        post(id : ID!) : Post!
     }
 
     schema{
